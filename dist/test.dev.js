d7e7e7e87d2fb3f2bc19a599bf4ca675282cdee0
jihad2j <jihad2j55@gmail.com>
2024-05-23 21:19:59 +0300

    hello

---
+++ dist/test.dev.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var _require = require('mongodb'),
+    MongoClient = _require.MongoClient,
+    ServerApiVersion = _require.ServerApiVersion;
+
+var uri = "mongodb+srv://zoolka:4XVbSXRHbsuku5WI@manager.0frmth1.mongodb.net/?retryWrites=true&w=majority&appName=manager"; // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+var client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1
+  }
+});
+
+function run() {
+  return regeneratorRuntime.async(function run$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(client.connect());
+
+        case 3:
+          _context.next = 5;
+          return regeneratorRuntime.awrap(client.db("manager").command({
+            ping: 1
+          }));
+
+        case 5:
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        case 6:
+          _context.prev = 6;
+          _context.next = 9;
+          return regeneratorRuntime.awrap(client.close());
+
+        case 9:
+          return _context.finish(6);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0,, 6, 10]]);
+}
+
+run()["catch"](console.dir);
+//# sourceMappingURL=test.dev.js.map
